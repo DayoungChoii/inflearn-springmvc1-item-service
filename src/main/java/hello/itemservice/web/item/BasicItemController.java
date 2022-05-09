afad907e12a1dce4c51624b0790668ae5c9b70df
DayoungChoii <young12430@gmail.com>
2022-05-09 11:33:54 +0900

    [springmvc1] 상품 목록 - 타임리프

---
+++ src/main/java/hello/itemservice/web/item/BasicItemController.java
@@ -0,0 +1,37 @@
+package hello.itemservice.web.item;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        ArrayList<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     * 의존성 주입이 이루어진 후 초기화를 수행하는 메서드
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+}
